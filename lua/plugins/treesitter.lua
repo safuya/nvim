0dfecb3ca5b15b177427e4d1e029c71d33058d7a
Sophie Hughes <sophie.hughes@nwboxed.com>
2026-04-19 09:46:20 +0100

    Re-enable markdown in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,7 +12,6 @@ return {
       highlight = true, -- enable/disable treesitter based highlighting
       indent = true, -- enable/disable treesitter based indentation
       auto_install = true, -- enable/disable automatic installation of detected languages
-      disable = { "markdown", "markdown_inline" }, -- fall back to syntax highlighting for markdown until TS crash is fixed
       ensure_installed = {
         "lua",
         "vim",
